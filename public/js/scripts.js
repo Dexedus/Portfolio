c2e086b21c72f51ee7792fb4dbd303bca734a777
Dexedus <karlfleming64@gmail.com>
2024-10-24 15:14:45 +0100

    add second animation set to about page, make projects page more responsive

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,10 @@ $(".submit").on("click", function(){
     alert("Your submission will be sent to Karl")
 
 })
+
+$(".lil").on('click', function(){
+    this.classList.add("Click")
+    setTimeout(() => {
+        this.classList.remove("Click")
+    }, 1000);
+})
